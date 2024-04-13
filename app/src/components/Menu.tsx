d749aa54f05280b8ed024fe74b7657838b7867fa
Coulton.Fraser <coulton.fraser@ovintiv.com>
2024-04-13 13:22:25 -0600

    added loading icons onto the menu

--- app/src/components/Menu.tsx
+++ app/src/components/Menu.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import { inviteAtom } from "@/components/inviteStore.ts";
 import { classNames } from '@/functions/classNames.ts';
+import {LoadingIcon} from './LoadingIcon.tsx';
 
 
 export default function Menu({pathname, menuImage}: {pathname:string, menuImage: any}) {
@@ -10,15 +11,21 @@ export default function Menu({pathname, menuImage}: {pathname:string, menuImage:
   const [navigation] = React.useState([
     { name: 'Gallery', href: '/', id: 'gallery-menu-href' },
     { name: 'R.S.V.P.', href: `/invite${inviteId.length>0 ? '/' + inviteId:''}`, id: 'invite-menu-href'},
-    { name: 'Schedule', href: '/schedule', id: 'schedule-menu-href'},
+    { name: 'Schedule', href: `/schedule${inviteId.length > 0 ? '/' + inviteId : ''}`, id: 'schedule-menu-href'},
     { name: 'Registry', href: `/registry${inviteId.length > 0 ? '/' + inviteId : ''}`, id: 'registry-menu-href'},
     { name: 'Trivia', href: `/trivia${inviteId.length>0 ? '/' + inviteId : ''}`, id: 'trivia-menu-href'},
-    { name: 'FAQ', href: '/faq', id: 'faq-menu-href'},
-    { name: 'Save The Date', href: '/save-the-date', id: 'save-the-date-menu-href'},
+    { name: 'FAQ', href: `/faq${inviteId.length>0 ? '/' + inviteId : ''}`, id: 'faq-menu-href'},
+    // { name: 'Save The Date', href: '/save-the-date', id: 'save-the-date-menu-href'},
   ])
   React.useEffect(() => {
     setMobileMenuOpen(false)
   },[])
+  const setClicked = (name: string) => {
+    setTimeout(() => {
+      setClickedState(name)
+    }, 250)
+  } 
+  const [clicked, setClickedState] = React.useState<string | null>(null)
 
   return (
     <header className="w-full border-b border-slate-500 ">
@@ -41,9 +48,16 @@ export default function Menu({pathname, menuImage}: {pathname:string, menuImage:
         </div>
         <div className="hidden md:flex justify-center md:gap-x-12">
           {navigation.map((item) => (
-            <a key={item.name} id={item.id} href={item.href} className={classNames("text-sm  leading-6 text-gray-900", pathname === item.href && 'font-bold')}>
+            <div className='relative'>
+            <a key={item.name} id={item.id} onClick={()=>setClicked(item.name)} href={item.href} className={classNames("text-sm  leading-6 text-gray-900", pathname === item.href && 'font-bold', clicked === item.name && 'font-bold')}>
               {item.name}
             </a>
+            {clicked === item.name && (<div className='border border-rosette-400 absolute inset-y-0 right-0 left-0 bg-rose-300 bg-opacity-30 rounded-lg justify-end items-center pr-2 flex -mr-6 -mx-3'>
+              <div className='fill-rosette-500 h-3 w-3'>
+                <LoadingIcon/>
+              </div>
+            </div>)}
+            </div>
           ))}
         </div>
       </nav>
@@ -72,15 +86,24 @@ export default function Menu({pathname, menuImage}: {pathname:string, menuImage:
             <div className="h-full -my-6 divide-y divide-gray-500/10">
               <div className="flex h-full flex-col space-y-2 pt-6">
                 {navigation.map((item) => (
+                  <div className='relative'>
                   <a
                     key={item.name}
                     id={`${item.id}-mobile`}
                     href={item.href}
                     className={classNames("-mx-3 block rounded-lg px-3 py-2 text-base leading-7 text-gray-900 hover:bg-rosette-100",
-                      pathname === item.href && ' underline font-bold')}
+                      pathname === item.href && ' underline font-bold',
+                      item.name === clicked && ' underline font-bold')}
                   >
                     {item.name}
                   </a>
+                  {clicked === item.name && (<div className='border border-rosette-400 absolute inset-y-0 right-0 left-0 bg-rose-300 bg-opacity-30 rounded-lg justify-end items-center pr-2 flex -ml-3'>
+                    <div className='fill-rosette-500 h-4 w-4'>
+                      <LoadingIcon/>
+                    </div>
+                  </div>
+                )}
+              </div>
                 ))}
 
                 <div className="flex-grow overflow-hidden">
